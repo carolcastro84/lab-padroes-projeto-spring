f8f63934c9c7d73a07129d94f6cb26f250fea0e5
Caroline <carolcastro84@gmail.com>
2022-08-31 21:41:08 -0300

    commit do projeto

---
+++ src/main/java/model/Cliente.java
@@ -0,0 +1,28 @@
+package model;
+
+import javax.persistence.*;
+
+@Entity
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @ManyToOne
+    private Endereco endereco;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+}
